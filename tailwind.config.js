274fb12aabc6163efded2465fabbf4cb920fb190
Monira Jahan <monirajahan459@gmail.com>
2023-08-13 20:54:05 +0600

    Card Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@
 module.exports = {
   content: [
     "./index.html",
+    "./index1.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll: 'scroll 40s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-250px * 14))' },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
 
